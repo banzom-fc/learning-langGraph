1c684084b889844245a80c247300ffc556ac272d
Aditya Panwar <aditya.panwar.2208@gmail.com>
2025-06-02 14:03:44 +0530

    Agent 1: Simple AI Invoking

---
+++ AIAgents/agent1.py
@@ -0,0 +1,42 @@
+from typing import TypedDict, List
+from langgraph.graph import StateGraph, START, END
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+# print("API: ", os.getenv("GEMINI_API_KEY"))
+
+class AgentState(TypedDict):
+    messages: List[HumanMessage]
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", google_api_key=os.getenv("GEMINI_API_KEY"))
+
+#node
+def chat(state: AgentState) -> AgentState:
+    """
+    Chat node that takes the current state, invokes the LLM, and appends the response to the messages.
+    Args:
+        state (AgentState): The current state containing messages.
+    Returns:
+        AgentState: The updated state with the new message appended.
+    """
+    response = llm.invoke(state["messages"])
+    state["messages"].append(HumanMessage(content=response.content))
+    return state
+
+#state graph
+graph = StateGraph(AgentState)
+graph.add_node("chat", chat)
+
+graph.add_edge(START, "chat")
+graph.add_edge("chat", END)
+
+agent = graph.compile()
+
+# Run the graph with an initial state
+query = input("You: ")
+initial_state = AgentState(messages=[HumanMessage(content=query)])
+final_state = agent.invoke(initial_state)
+print("AI: ", final_state["messages"][-1].content)

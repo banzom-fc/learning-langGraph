a6b176220aa8f12e0fbfd4088b0ffada4b356902
banzom-fc <aditya.01.in@freecharge.in>
2025-06-22 19:14:40 +0530

    Agent 3: Enhance model_node to maintain context with state messages and add condition_node for tool execution logic

--- AIAgents/agent3.py
+++ AIAgents/agent3.py
@@ -53,6 +53,7 @@ def add_numbers(a: float, b: float) -> float:
 tools = [
     add_numbers,
 ]
+tool_node = ToolNode(tools=tools)
 
 # DEBUG 
 #print(f"DEBUG: GEMINI_API_KEY loaded: {os.getenv('GEMINI_API_KEY')}")
@@ -72,9 +73,72 @@ def model_node(state: AgentState) -> AgentState:
     system_prompt = SystemMessage(
         content="You are a helpful assistant."
     )
-    response = model.invoke([system_prompt])
+    response = model.invoke([system_prompt] + state["messages"])  # Use the messages from the state to maintain context
     
     
     return {
         "messages": [response]
     } # this type of return can be used to update the state with new messages along with the existing ones. Because of the add_messages decorator, it will automatically merge the new messages with the existing ones in the state.
+    
+
+# Condition Node
+def condition_node(state: AgentState) -> str:
+    """
+    Checks the last message in the state to determine if it contains tool calls.
+    If it does, it returns "continue_with_tool" to indicate that the tool should be executed.
+    If not, it returns "end" to indicate that the conversation should end.
+    """
+    last_message = state["messages"][-1]  # Get the last message in the sequence
+    
+    if last_message.tool_calls:
+        return "continue_with_tool"
+    else:
+        return "end"
+    
+
+# State Graph Structure
+graph = StateGraph(AgentState)
+
+graph.add_node("MODEL", model_node)  
+graph.add_node("TOOL", tool_node)
+
+graph.add_edge(START, "MODEL")
+graph.add_conditional_edges(
+    "MODEL", 
+    condition_node, 
+    {
+        "continue_with_tool": "TOOL",
+        "end": END
+    }
+)
+graph.add_edge("TOOL", "MODEL")  # Loop back to the model node after tool execution
+
+# Compile the agent
+agent = graph.compile()
+
+# Save the graph image to a file
+image_bytes = agent.get_graph().draw_mermaid_png()
+image_path = os.path.join(os.getcwd(), "AIAgents", "imgs", "agent-3-graph.png")
+with open(image_path, "wb") as f:
+    f.write(image_bytes)
+
+# Helper function to convert inputs to BaseMessage format
+def print_stream(stream):
+    for s in stream:
+        message = s["messages"][-1]  # Get the last message in the stream
+        if isinstance(message, tuple):
+            print(message)
+        else:
+            message.pretty_print()
+
+
+# Run the graph
+inputs = {
+    "messages": [
+        (
+            "user", "Hello! Can you help me with some math? What is the Additon fo 3 and 9.0009 and what is subtraction for 8 and 0.999",
+        )
+    ]
+}
+print_stream(agent.stream(inputs, stream_mode="values"))
+
